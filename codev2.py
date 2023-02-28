33b68bce7cbb3d8d131887dbeb2d9d940ab90eb4
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2023-02-28 15:33:18 -0800

    Update codev2.py

--- codev2.py
+++ codev2.py
@@ -1,9 +1,9 @@
+# PROJECT CODE
 import board
 import pwmio
 from adafruit_circuitplayground import cp
 import adafruit_motor.servo
 from time import sleep
-import random
 
 # Setup Section
 pwm = pwmio.PWMOut(board.A1, frequency=50)
@@ -12,14 +12,14 @@ servo = adafruit_motor.servo.Servo(pwm, min_pulse=750, max_pulse=2600)
 
 # User Specified Max Time (minutes)
 
-MAX = 5
+MAX = 0.5
 
 # Timer Variables
 MAX_TIME = MAX * 60
 timer = 0
 interval = 0
 count = 0
-
+cp.pixels.brightness = 0.1
 # Loop Section
 
 while True:
@@ -33,17 +33,17 @@ while True:
         timer += MAX_TIME/10
         interval += 1
         cp.pixels[interval-1] = (0, 255, 0)
-        sleep(0.5)
+        sleep(0.25)
         print("WILL LOCK FOR", timer/60, "MINUTES")
-    # SET LOCK    
+    # SET LOCK
     elif cp.button_b:
         print("LOCKED FOR", timer/60, "MINUTES")
         # LOCKING ANIMATION
         for i in range (0, interval):
             cp.pixels[i] = (255, 0, 0)
-            sleep(0.25)
-        servo.angle = 180
-        
+            sleep(0.1)
+        servo.angle = 90
+
         # TIMER
         while (timer != 0):
             sleep(1)
@@ -61,7 +61,7 @@ while True:
         servo.angle = 0
         timer = 0
         cp.pixels.fill((0,255,0))
-        sleep(0.25)
+        sleep(0.5)
         cp.pixels.fill((0,0,0))
         print("UNLOCKED")
     else:
